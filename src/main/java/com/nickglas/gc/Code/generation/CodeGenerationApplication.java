9eafecdf579a7329975233271db4a6fdbea83e52
Nick Glas <nickglas@hotmail.nl>
2022-05-01 02:16:40 +0200

    Removed old commandLineRunner - Removed runner function in main application entry(is seperate now)

--- src/main/java/com/nickglas/gc/Code/generation/CodeGenerationApplication.java
+++ src/main/java/com/nickglas/gc/Code/generation/CodeGenerationApplication.java
@@ -20,19 +20,6 @@ public class CodeGenerationApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(TransactionService transactionService){
-		return args -> {
-			Transaction Trans1 = transactionService.createTransaction(new Transaction("IBNA_000_FROM", "IBNA_001_TO", TransactionType.transaction));
-			Transaction Trans2 = transactionService.createTransaction(new Transaction("IBNA_001_TO", "IBNA_001_TO", TransactionType.deposit));
-			Transaction Trans3 = transactionService.createTransaction(new Transaction("IBNA_004_FROM", "IBNA_004_FROM", TransactionType.withdraw));
-			log.info("Created test transaction {}" , Trans1.toString());
-			log.info("Created test transaction {}" , Trans2.toString());
-			log.info("Created test transaction {}" , Trans3.toString());
-			//transactionService.createTransaction(new TransactionTransfer("ibanfrom", "ibanto", TransactionType.transaction));
-		};
-	}
-
-	@Bean
 	PasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
 	}
